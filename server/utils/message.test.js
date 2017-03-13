71a45d009e98ac4b99a9aa6c5291933a5f1687d2
Aritra Mukherjee <aritradmukherjee@gmail.com>
2017-03-13 08:46:17 +0530

    geolocation complete

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -12,3 +12,14 @@ describe('generateMessage', function() {
     expect(message).toInclude({from, text});
   });
 });
+describe('generateLocationMessage', () =>{
+  it('should generate correct location object', function() {
+    var from = 'aritra';
+    var latitude = '20';
+    var longitude = "10";
+    var url="https://www.google.com/maps?q=15,19";
+    var message=generateLocationMessage(from,latitude,longitude);
+      expect(message.createdAt).toBeA('number');
+    expect(message).toInclude({from, url});
+  });
+});
